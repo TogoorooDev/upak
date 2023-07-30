bd1c900711eeb9f7792ec8f617f1989a2256dec3
Sasha Renée <lhdev25@gmail.com>
2023-07-30 14:10:12 -0700

    fixed some errors in pack.sh, added usage instructions

--- pack.sh
+++ pack.sh
@@ -1,58 +1,72 @@
 #! /bin/bash
 
-# if [ "$1" = "--se" || "$2" == "--se" || "$3" == "--se" || "$4" = "--se" ];
+usage () {
+  echo "usage: ./pack.sh FILENAME --se|--standard --quiet|--loud --no-compress|-compress"
 
-if [ "$FILE" = "" ]; then
-  FILE="upakbin"
-fi
+  echo "FILENAME: Path to executable to pack"
+  echo ""
+  
+  echo "--se: Security Enahnced Binary. Blocks ptrace calls and encrypts the executable (ENCRYPTION NOT IMPLEMENTED)"
+  echo "--standard: Standard Security Binary. None of these features"
+  
+  echo ""
+  
+  echo "--quiet: Disables logging, but keeps error messages"
+  echo "--loud: Enables debug logging and error messages"
 
+  echo ""
+  
+  echo "--no-compress: Disables ZSTD compression of executable"
+  echo "--compress: Enables ZSTD compression of executable"
+
+  echo ""
+
+  exit 0;
+}
+  
 echo "UPAK Packed File Creator"
-if [ "$1" != "" ]; then
+
+if [ "$1" != "" ]; then  
   FILENAME="$1"
 else
-  read -p "Please Enter In File To Pack: " FILENAME
+  usage
 fi
 
-if [ "$2" = "--secure" ] || [ "$3" = "-s" ]; then
+if [ "$2" = "--se" ]; then
   ENCRYPTION=1
   SE=" -DSECURITY_ENHANCED "
   echo "Security Enhanced Binary"
   read -p "Enter encryption passphrase: " -s PASSWORD
   printf "\n$PASSWORD\n"
+
+elif [ "$2" = "--standard" ]; then
+    ENCRYPTION=0
+    SE=""
+    echo "Standard Security Binary"
+
+else
+  usage
 fi
 
-if [ "$3" = "--quiet" ] || [ "$3" = "-q" ]; then
+if [ "$3" = "--quiet" ]; then
   SE+=" -DQUIET "
   echo "Quiet Binary"
+elif [ "$3" = "--loud" ]; then
+  echo "Loud Binary"
+else 
+  usage
 fi
 
 test -d "obj" || mkdir obj
 
-
 if test -f "$FILENAME"; then
   echo "Packing file: $FILENAME"
   # cp "$FILENAME" obj/executable.
-  
-  if [ "$4" = "--no-compress" ] || [ "$4" = "-n" ]; then
-    C=" -DNO_COMPRESS "
-    cp "$FILENAME" obj/executable.tmp
-  else
-    zstd "$FILENAME" -o obj/executable.tmp
-  fi
-  FILE="$FILE" CFLAGS="$SE $C" make
-
-  UNPAKFS=$(stat --printf="%s" obj/executable.tmp)
-  PAKFS=$(stat --printf="%s" "$FILE")
-
-  if [ $PAKFS -gt $UNPAKFS ]; then
-    echo "NOTICE: Packed file is larger than unpacked file"
-  fi
-
+  zstd "$FILENAME" -o obj/executable.tmp
+  CFLAGS="$SE" make
   make genclean
 
   echo "This should be complete"
-
-else 
-  echo "$FILENAME does not exist..."
 fi
+
 
